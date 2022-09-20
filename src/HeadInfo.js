85200faed17a05a05a88f6818b3cfc94e9856052
Robarto <robartpetrov@gmail.com>
2022-09-20 17:36:55 +0300

    Added Header

---
+++ src/HeadInfo.js
@@ -0,0 +1,15 @@
+const HeadInfo = () => {
+  return (
+    <div className="headInfo">
+      <h1>Unlimited movies, TV shows, and more.</h1>
+      <h2>Watch anywhere. Cancel anytime.</h2>
+      <h3>
+        Ready to watch? Enter your email to create or restart your membership.
+      </h3>
+      <input type="email" placeholder="Email Address" />
+      <button>Get Started {">"}</button>
+    </div>
+  );
+};
+
+export default HeadInfo;
